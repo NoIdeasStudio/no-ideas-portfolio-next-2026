eccd330e617cfeaef43f079dc90ce24b7aff004f
Philip DiBello <pmdibello@gmail.com>
2026-06-23 13:07:22 -0400

    dvh viewport update

--- src/components/ProjectCarousel.tsx
+++ src/components/ProjectCarousel.tsx
@@ -207,7 +207,7 @@ export function ProjectCarousel({
   return (
     <section
       id={isLoopCopy || omitSectionId ? undefined : (projectSlug ?? undefined)}
-      className="hero-slider relative h-screen w-full overflow-hidden bg-black"
+      className="hero-slider project-viewport-height relative w-full overflow-hidden bg-black"
       aria-label={`Project: ${projectTitle}`}
     >
       <div className="relative h-full w-full">
