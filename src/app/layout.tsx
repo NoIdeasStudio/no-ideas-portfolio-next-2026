8157517e8f8d05a756f23b055b09ad2baf0d621d
Philip DiBello <pmdibello@gmail.com>
2026-07-24 16:25:40 -0400

    Restoring Sanity Live for instant Updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -59,7 +59,7 @@ export default async function RootLayout({ children }: { children: ReactNode }) 
             </PageWrapper>
           </AutoScrollProvider>
         </ProjectThemeProvider>
-        {isDraftMode && <SanityLive />}
+        <SanityLive />
         {isDraftMode && (
           <>
             <DisableDraftMode />
